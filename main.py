fda6242a8ea19ecb578b62cbddc9654ace7877ca
whalegaush-alt <whale.gaush@gmail.com>
2026-02-06 16:59:37 +0100

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(
+        types.KeyboardButton(
+            text="🛒 Открыть магазин",
+            web_app=types.WebAppInfo(
+                url="https://example.com"  # пока заглушка
+            )
+        )
+    )
+
+    await message.answer(
+        "Добро пожаловать в онлайн-магазин 🍔🥦\n"
+        "Продукты и готовая еда",
+        reply_markup=keyboard
+    )
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
