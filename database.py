0c6f879e43894a8a769bdf897de128e711ecda92
whalegaush-alt <whale.gaush@gmail.com>
2026-02-07 22:55:32 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Railway сам выдаст DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    price = Column(Float)
+    photo_url = Column(String)
+
+# Создаем таблицы
+Base.metadata.create_all(bind=engine)
 
