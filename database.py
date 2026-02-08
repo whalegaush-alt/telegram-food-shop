dca6eace63b2211ed1c2e6823965d85d1e907194
whalegaush-alt <whale.gaush@gmail.com>
2026-02-08 02:24:47 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
@@ -18,6 +17,6 @@ class Item(Base):
     name = Column(String)
     price = Column(Float)
     photo_url = Column(String)
-    category = Column(String, default="Другое") # Поле категории
+    category = Column(String, default="Другое")
 
 Base.metadata.create_all(bind=engine)
